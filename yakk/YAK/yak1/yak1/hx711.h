d668f3e25592da05081dc2b234da15350ef28093
Establers <44702967+Establers@users.noreply.github.com>
2020-07-23 12:02:52 +0900

    Add files via upload

---
+++ yakk/YAK/yak1/yak1/hx711.h
@@ -0,0 +1,18 @@
+﻿/*
+ * hx711.h
+ *
+ * Created: 2020-07-14 오후 4:20:22
+ *  Author: ParkJaeHwan
+ */ 
+
+
+#ifndef HX711_H_
+#define HX711_H_
+#include <avr/io.h>
+
+void weight_init();
+unsigned long  ReadCout(void);
+void init();
+
+
+#endif /* HX711_H_ */
